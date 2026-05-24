baefd1320a5fe0b38e37cc8b617cdd9a62d3b3ae
Rabtly CI <ci@rabtly.cloud>
2026-05-24 11:32:54 +0000

    chore: bump to v1.4.10.040

--- Formula/rabtly.rb
+++ Formula/rabtly.rb
@@ -1,16 +1,16 @@
 class Rabtly < Formula
   desc "Self-hosted mesh VPN built on WireGuard"
   homepage "https://rabtly.cloud"
-  version "1.4.10.038"
+  version "1.4.10.040"
 
   on_macos do
     on_arm do
-      url "https://dl.rabtly.cloud/v1.4.10.038/rabtly_darwin_arm64"
-      sha256 "a86f25eeeb565d095282a3494d77ccd6178f25759281149fe4d28627cce69249" # arm64
+      url "https://dl.rabtly.cloud/v1.4.10.040/rabtly_darwin_arm64"
+      sha256 "e4255b72aa7dd7756de6c22ca8bbeff7d76cc46a5cc6b881810990822013da14" # arm64
     end
     on_intel do
-      url "https://dl.rabtly.cloud/v1.4.10.038/rabtly_darwin_amd64"
-      sha256 "a6eb7bd417a7d6cf1e01dd63f56ff9a657e1c996d4ef7e27bb38cb4a35d0aef6" # amd64
+      url "https://dl.rabtly.cloud/v1.4.10.040/rabtly_darwin_amd64"
+      sha256 "2c8aad1fe3f5cf1c535b3e97b0723d686d3b421f651ab736225987b423c787b8" # amd64
     end
   end
 
