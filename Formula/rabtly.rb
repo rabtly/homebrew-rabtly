b57de595f26d026a3abd8f0219462f1a05210f25
Rabtly CI <ci@rabtly.cloud>
2026-05-29 11:36:17 +0000

    chore: bump to v1.4.10.052

--- Formula/rabtly.rb
+++ Formula/rabtly.rb
@@ -6,11 +6,11 @@ class Rabtly < Formula
   on_macos do
     on_arm do
       url "https://dl.rabtly.cloud/v1.4.10.052/rabtly_darwin_arm64"
-      sha256 "40fe96f9ff70b649e8021ec9617acffcca918bb50b6677d1594610b00ce23ec7" # arm64
+      sha256 "7032277c50d1eddd9719a4e834eb3c11ecc6ab147d77fabbf2c3deb79750e959" # arm64
     end
     on_intel do
       url "https://dl.rabtly.cloud/v1.4.10.052/rabtly_darwin_amd64"
-      sha256 "502fccadcbee1e4b6ddc6a593b3c8eea6a6a8cf186bcdad0c38bd3144e9fef49" # amd64
+      sha256 "4daec5722f58770c1e2cb02b5329a2feabb544e1fec8c0f2f6c56e6dedc1c4a2" # amd64
     end
   end
 
